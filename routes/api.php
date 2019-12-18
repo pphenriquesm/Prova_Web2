de299d85c66d3710c55c697fa7e459fb28299f4e
GuilhermeH6 <guilhermeholanda870@gmail.com>
2019-12-18 14:42:13 -0300

    fazendo as rotas

--- routes/api.php
+++ routes/api.php
@@ -16,22 +16,19 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-    Route::namespace('api')->name('api.')->group(function(){
+Route::namespace('Api')->name('api.')->group(function(){
 	Route::prefix('sensor')->group(function(){
-		
 		Route::get('/', 'SensorController@index')->name('index_sensor');
 		Route::get('/{id}', 'SensorController@show')->name('single_sensor');
 		Route::post('/', 'SensorController@store')->name('store_sensor');
 		Route::put('/{id}', 'SensorController@update')->name('update_sensor');
 		Route::delete('/{id}', 'SensorController@delete')->name('delete_sensor');
-});
-	Route::prefix('medicao')->group(function(){
-		
-		Route::get('/', 'MedicaoController@index')->name('index_medicao');
-		Route::get('/{id}', 'MedicaoController@show')->name('single_medicao');
-		Route::post('/', 'MedicaoController@store')->name('store_medicao');
-		Route::put('/{id}', 'MedicaoController@update')->name('update_medicao');
-		Route::delete('/{id}', 'MedicaoController@delete')->name('delete_medicao');
-		});
+    });
+    Route::prefix('medicao')->group(function(){
+        Route::get('/', 'MedicaoController@index')->name('index_medicao');
+        Route::post('/', 'MedicaoController@store')->name('store_medicao');
+        Route::get('/{id}', 'MedicaoController@show')->name('single_medicao');
+        Route::put('/{id}', 'MedicaoController@update')->name('update_medicao');
+        Route::delete('/{id}', 'MedicaoController@delete')->name('delete_medicao');
+    });
 });
